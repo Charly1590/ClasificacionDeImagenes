2c119433f333b95787a28ff5a69eb619785caca9
FlorencioP <sebasale10@hotmail.com>
2020-12-13 21:38:30 -0500

    MANI

--- ProyectoInterciclo/ProyectoInterciclo/settings.py
+++ ProyectoInterciclo/ProyectoInterciclo/settings.py
@@ -137,5 +137,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), 
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
